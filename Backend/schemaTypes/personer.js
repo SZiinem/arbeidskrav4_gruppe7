9ae15c65a75f89ca4c6168f9366a629c90495b75
SZiinem <sineme@hiof.no>
2026-04-17 13:36:49 +0200

    Opprettet schema for personer og arbeidskrav for sanity

---
+++ Backend/schemaTypes/personer.js
@@ -0,0 +1,29 @@
+const personer = {
+    name: "person",
+    title: "Gruppemedlemmer",
+    type: "document",
+    fields: [
+        {
+            name: "medlemimage",
+            title: "Bilde",
+            type: "image"
+        },
+        {
+            name: "name",
+            title: "Navn",
+            type: "string"
+        },
+        {
+            name: "email",
+            title: "Epost",
+            type: "email"
+        },
+        {
+            name: "course",
+            title: "Studie",
+            type: "string"
+        }
+    ]
+}
+
+export default personer
