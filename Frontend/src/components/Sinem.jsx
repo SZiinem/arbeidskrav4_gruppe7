cba6b4c0679c4942c19f3c2959cb537d6f27695b
SZiinem <sineme@hiof.no>
2026-04-22 11:31:21 +0200

    Hentet ut min info fra sanity og fikset css for min profil-visning

--- Frontend/src/components/Sinem.jsx
+++ Frontend/src/components/Sinem.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react"
+import client from "../helpers/client"
+import '../style/sinem.css'
+
 export default function Sinem({ }) {
-    return (
-        <h1>Sinem</h1>
+
+
+    const [sanitySinem, setSanitySinem] = useState(null)
+
+    useEffect(() => {
+        async function fetchAllSinem() {
+            const allSinem = await client.fetch("*[_type == 'person']{_id, 'image': medlemimage.asset->url, name, email, course}")
+            setSanitySinem(allSinem[1])
+        }
+        fetchAllSinem()
+    }, [])
+
+    console.log(sanitySinem)
+   
+   
+    return(
+        <>
+
+        {sanitySinem &&
+            (<article className="sinem">
+                <img src={sanitySinem.image ? sanitySinem.image : "https://placehold.co/300x300?text=Bilde+kommer"} alt="s.name"></img>
+                <h3>{sanitySinem.name}</h3>
+                <p>{sanitySinem.course}</p>
+                <a href={`mailto:${sanitySinem.email}`}>{sanitySinem.email}</a>
+            </article>)}
+        </>
     )
 }
