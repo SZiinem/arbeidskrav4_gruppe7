a5fde33f1a74432c1ed10b5993fa5b0e2b83c2d6
filipemamador <filipeamador@outlook.com>
2026-04-22 11:53:08 +0200

    Ferdig med Filipe komponentet

--- Frontend/src/components/Filipe.jsx
+++ Frontend/src/components/Filipe.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react"
+import client from "../helpers/client"
+import "../style/filipe.css"
+
 export default function Filipe({ }) {
 
+    const [filipeSanity, setFilipeSanity] = useState(null)
 
+    useEffect(() => {
+        async function fetchSanityFilipe() {
+            const sanityFilipe = await client.fetch("*[_type == 'person' && name == 'Filipe Amador']{_id, 'imageURL': medlemimage.asset->url, name, email, course}")
+            setFilipeSanity(sanityFilipe[0])
+        }
+        fetchSanityFilipe()
+    }, [])
+
+    console.log(filipeSanity)
     return (
-        <h1>Filipe</h1>
+        <article className="FilipeCard">
+            <img src={filipeSanity?.imageURL} alt={filipeSanity?.name} />
+            <h3>{filipeSanity?.name}</h3>
+            <a href={`mailto:${filipeSanity?.email}`}>{filipeSanity?.email}</a>
+            <p>{filipeSanity?.course}</p>
+        </article>
     )
 }
