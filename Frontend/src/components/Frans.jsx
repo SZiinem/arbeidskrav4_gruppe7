d1ab49aaaa0398ad8639df7071151637debf0988
noax1137 <fransnn@hiof.no>
2026-04-22 11:44:43 +0200

    sanity-fetch og jsx render i Frans.jsx

--- Frontend/src/components/Frans.jsx
+++ Frontend/src/components/Frans.jsx
@@ -1,5 +1,30 @@
-export default function frans(){
+import { useEffect, useState } from "react"
+import client from "../helpers/client"
+
+export default function Frans(){
+
+    const [sanityFrans, setSanityFrans] = useState(null)
+
+    useEffect(() => {
+        async function fetchFrans(){
+            const onlyFrans = await client.fetch("*[_type == 'person' && name == 'Frans N. Naas'][0]{_id, 'imageURL': medlemimage.asset->url, name, email, course}")
+            setSanityFrans(onlyFrans)
+        }
+        fetchFrans()
+    }, [])
+
+    console.log(sanityFrans)
+
     return(
-        <h1>Overskrift</h1>
+
+
+        <article className="frans-Card">
+            <h1>{sanityFrans?.name}</h1>
+            <img src={sanityFrans?.imageURL} alt="Bilde av hund" className="frans-bilde" />
+            <a href="">{sanityFrans?.email}</a>
+            <p>{sanityFrans?.course}</p>
+
+
+        </article>
     )
 }
